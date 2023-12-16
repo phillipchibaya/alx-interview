39dad19e1a78fa3a3c633db68d7d5137f87dbe5c
phillipchibaya <chibayaphillip@gmail.com>
2023-12-15 22:27:32 -0800

    Task 1

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rotate_2d_matrix(matrix)
+
+print(matrix)
